66dba82173fd737f00b4d39d80a21e0062f69333
Emeka Stanley <emy@meetkumba.com>
2024-04-18 17:14:12 +0100

    update

--- src/components/common/EachLineupContainer.tsx
+++ src/components/common/EachLineupContainer.tsx
@@ -3,6 +3,7 @@ import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import { useTheme } from 'next-themes'
 import { RadioTower } from 'lucide-react';
+import Link from 'next/link';
 
 export default function EachLineupContainer() {
     const { setTheme } = useTheme();
@@ -33,30 +34,34 @@ export default function EachLineupContainer() {
 }
 
 const EachLineup = ({ isLive }: { isLive?: boolean }) => {
-    return <div className='w-full flex items-center border-t md:gap-0 gap-3 cursor-pointer hover:bg-hover rounded-lg hover:shadow-sm'>
-        <div className='h-20 w-32 flex justify-center items-center'>
-            <small>22:00</small>
-        </div>
-        <div className='flex-1 flex items-center gap-4 justify-center'>
-            <div className="flex items-center gap-2">
-                <small className='truncate'>Manchester City</small>
-                <img className="crest team-crest_crest__Jp9_k" src="https://cdn.sportfeeds.io/sdl/images/team/crest/medium/a3nyxabgsqlnqfkeg41m6tnpp.png" alt="Manchester City" loading="lazy" decoding="async" height="32" width="32" />
+    return <Link href='/live/sports/football/1234'>
+        <div className='w-full flex items-center border-t md:gap-0 gap-3 cursor-pointer hover:bg-hover rounded-lg hover:shadow-sm'>
+            <div className='h-20 w-32 flex justify-center items-center'>
+                <small>22:00</small>
             </div>
-            <div className="flex gap-2">
-                <span>0</span>
-                <span>-</span>
-                <span>3</span>
+            <div className='flex-1 flex items-center gap-4 justify-center'>
+                <div className="flex items-center gap-2">
+                    <small className='truncate'>Manchester City</small>
+                    <img className="crest team-crest_crest__Jp9_k" src="https://cdn.sportfeeds.io/sdl/images/team/crest/medium/a3nyxabgsqlnqfkeg41m6tnpp.png" alt="Manchester City" loading="lazy" decoding="async" height="32" width="32" />
+                </div>
+                <div className="flex gap-2">
+                    <span>0</span>
+                    <span>-</span>
+                    <span>3</span>
+                </div>
+                <div className="flex items-center gap-2">
+                    <img className="crest team-crest_crest__Jp9_k" src="https://cdn.sportfeeds.io/sdl/images/team/crest/medium/a3nyxabgsqlnqfkeg41m6tnpp.png" alt="Manchester City" loading="lazy" decoding="async" height="32" width="32" />
+                    <small className='truncate'>Manchester City</small>
+                </div>
             </div>
-            <div className="flex items-center gap-2">
-                <img className="crest team-crest_crest__Jp9_k" src="https://cdn.sportfeeds.io/sdl/images/team/crest/medium/a3nyxabgsqlnqfkeg41m6tnpp.png" alt="Manchester City" loading="lazy" decoding="async" height="32" width="32" />
-                <small className='truncate'>Manchester City</small>
+            <div className='h-20 w-32 flex justify-center items-center'>
+                {isLive &&
+                    <small className='text-red-600 flex items-center gap-2'>
+                        <RadioTower className='text-red-600' />
+                        Live
+                    </small>
+                }
             </div>
         </div>
-        <div className='h-20 w-32 flex justify-center items-center'>
-            {isLive && <small className='text-red-600 flex items-center gap-2'>
-                <RadioTower className='text-red-600' />
-                Live
-            </small>}
-        </div>
-    </div>
+    </Link>
 }
