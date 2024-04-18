0846f5dea0c8a562189771077b17a3617891af79
Emeka Stanley <emy@meetkumba.com>
2024-04-18 21:47:28 +0100

    fix/build error

--- src/app/(home-page)/page.tsx
+++ src/app/(home-page)/page.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 import HomePage from './HomePage';
 import { baseUrl } from '@/constants';
 
+
+export const dynamic = 'force-dynamic';
+
 export default async function page() {
 
   const res = await fetch(`${baseUrl}/api/public/sports/football`)
