2a323fec9e14cfe9b6ed73500c87294853e796d2
Emeka Stanley <emy@meetkumba.com>
2024-04-22 10:18:16 +0100

    ..

--- src/app/(home-page)/page.tsx
+++ src/app/(home-page)/page.tsx
@@ -2,16 +2,16 @@ import React from "react";
 
 import HomePage from "./HomePage";
 import { baseUrl } from "@/constants";
+import axios from "axios";
 
 export const revalidate = 30;
 
 export default async function page() {
-    const res = await fetch(`${baseUrl}/api/public/sports/football`);
-    const data = await res.json();
+    const res = await axios(`${baseUrl}/api/public/sports/football`);
 
     return (
         <>
-            <HomePage football={data.tournaments} />
+            <HomePage football={res.data.tournaments} />
         </>
     );
 }
