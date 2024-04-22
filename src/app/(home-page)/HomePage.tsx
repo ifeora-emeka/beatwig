a76bcbdefcc5f05a464f5cb4a361e5092ad66569
Emeka Stanley <emy@meetkumba.com>
2024-04-22 10:16:42 +0100

    ..

--- src/app/(home-page)/HomePage.tsx
+++ src/app/(home-page)/HomePage.tsx
@@ -12,7 +12,6 @@ export default function HomePage({
 }: {
     football: FootballTournament[];
 }) {
-    console.log("HOME DATA::", football);
     return (
         <div className="flex justify-center">
             <Container>
