ecda59a8516c5aa2a8368cac43386132753bcbd4
Emeka Stanley <emy@meetkumba.com>
2024-04-22 10:07:19 +0100

    feature/details page

--- src/app/live/sports/football/[match_id]/FootballComments.tsx
+++ src/app/live/sports/football/[match_id]/FootballComments.tsx
@@ -13,16 +13,7 @@ export default function FootballComments({}: Props) {
                         "h-10 bg-green-400 w-full flex-1 overflow-y-auto overflow-x-hidden"
                     }
                 >
-                    <h1 className={"text-5xl"}>
-                        I am using Redux Toolkit to handle all the data. Every
-                        page of my application has it's own singular state.
-                        State along with Metadata is fetched/populated at page
-                        load. Now i want to set Metadata based on the data
-                        present in state. I've already tried generateMetadata
-                        provided by nextjs. I have hooks defined in my project
-                        which gets the data from state based on current route
-                        path. Is there something that i'm missing?
-                    </h1>
+
                 </div>
                 <div className={"h-10 bg-orange-400 w-full"}></div>
             </div>
