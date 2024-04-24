0f4c7bf2efd7c9a893f38fb53651f1fe60f8304b
Emeka Stanley <emy@meetkumba.com>
2024-04-24 10:23:48 +0100

    feature/live chat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Providers from "@/Providers";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const font = Poppins({
     weight: ["100", "300", "400", "700", "900"],
@@ -21,6 +22,7 @@ export default function RootLayout({
     return (
         <html lang="en" suppressHydrationWarning className="select-none">
             <body className={font.className}>
+                <SpeedInsights />
                 <Providers>{children}</Providers>
             </body>
         </html>
