2a647b90a3eeef7aac6ac8f1a0e768471ee0100c
Emeka Stanley <emy@meetkumba.com>
2024-04-25 22:35:54 +0100

    feature/landing page

--- src/components/common/nav/HomeHeader.tsx
+++ src/components/common/nav/HomeHeader.tsx
@@ -2,37 +2,43 @@
 import { Card } from "@/components/ui/card";
 import React from "react";
 import BrandLogo from "../BrandLogo";
-import { CalendarSearch, RefreshCwIcon, Search } from "lucide-react";
+import { User, RefreshCwIcon, Search } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next13-progressbar";
+import { useAuthContext } from "@/context/auth.context";
+
 
 type Props = {};
 
 export default function HomeHeader({}: Props) {
     const router = useRouter();
+    const { user } = useAuthContext();
 
     return (
         <Card className="px-5 py-3 z-50 flex gap-5 justify-between sticky top-0">
             <BrandLogo size={35} />
             <div className="flex gap-5 items-center">
-                <div className="flex items-center border rounded-md p-2 gap-3">
-                    <Search className="text-muted h-5 w-5" />{" "}
-                    <input
-                        className="placeholder:text-muted outline-none bg-inherit w-full"
-                        type="search"
-                        placeholder="Search fixtures..."
-                    />
-                </div>
-                <div className="min-w-10">
                     <Button
                         onClick={() => router.refresh()}
                         variant="outline"
                         size="icon"
                         className="bg-inherit text-muted"
                     >
-                        <RefreshCwIcon className="h-5 w-5" />
+                        <Search className="h-5 w-5" />
                     </Button>
-                </div>
+                {
+                    user ? <div className={'h-10 w-10 overflow-hidden rounded-full'}>
+                            <img src={user.avatar_url} alt={user.display_name} />
+                        </div> :
+                        <Button
+                            onClick={() => router.refresh()}
+                            variant="outline"
+                            size="icon"
+                            className="bg-inherit text-muted"
+                        >
+                            <User className="h-5 w-5" />
+                        </Button>
+                }
             </div>
         </Card>
     );
