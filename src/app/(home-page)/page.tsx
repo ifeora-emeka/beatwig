2d9856e5654150052bdafdc8407de7b331bb79c7
Emeka Stanley <emy@meetkumba.com>
2024-04-29 21:54:38 +0100

    added revalidation

--- src/app/(home-page)/page.tsx
+++ src/app/(home-page)/page.tsx
@@ -3,12 +3,15 @@ import React from "react";
 import HomePage from "./HomePage";
 import { baseUrl } from "@/constants";
 import axios from "axios";
+import { revalidatePath } from "next/cache";
 
 export const dynamic = 'force-dynamic'
 // export const revalidate = 930;
 
 export default async function page() {
     const res = await axios(`${baseUrl}/api/public`);
+
+    revalidatePath('/')
 
     return (
         <>
